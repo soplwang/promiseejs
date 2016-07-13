52ebb5f80b5a17d6919f89b1a952823ae88f99c6
Sopl’Wang <sopl.wang@gmail.com>
2016-07-14 04:38:46 +0800

    Initial import

---
+++ README.md
@@ -0,0 +1,29 @@
+promisee.js, Adapt promise with callback paradigm.
+====
+
+Desiged intentional works with `co`.
+
+```javascript
+const co = require('co');
+const _ = require('promisee');
+
+co(function* () {
+  var r = _(), r2 = _();
+  redis.get('r:1', r);
+  redis.get('r:2', r2);
+  console.log('r:1', yield r);
+  console.log('r:2', yield r2);
+  console.log('r:1 + r:2', (yield r) + (yield r2));
+});
+```
+
+Works without `co` like plain old `Promise` too:
+
+```javascript
+var r = _();
+
+redis.get('r:1', r);
+
+r.then(...)
+ .catch(...);
+```
---
+++ test/unit.js
@@ -0,0 +1,61 @@
+/* Copyright 2016, Wang Wenlin */
+"use strict";
+
+const assert = require('assert');
+const util = require('util');
+const co = require('co');
+const _ = require('../');
+
+describe('promisee.js', function () {
+  describe('promisee mode', function () {
+    it('normal usages w/ co', function (done) {
+      co(function* () {
+        var r = _(), r2 = _();
+        process.nextTick(() => r(null, 'ok'));
+        process.nextTick(() => r2('err'));
+        assert.equal(yield r, 'ok');
+        assert.equal((yield r) + '!', 'ok!');
+        try {
+          yield r2;
+        } catch (e) {
+          return done();
+        }
+        throw Error('yield not throw');
+      }).catch(done);
+    });
+
+    it('plain old promise', function (done) {
+        var r = _(), r2 = _();
+        process.nextTick(() => r(null, 'ok'));
+        process.nextTick(() => r2('err'));
+        r.then(v => {
+          assert.equal(v, 'ok');
+          r.then(v => {
+            assert.equal(v + '!', 'ok!');
+            r2.then(v => done(Error('r2 not throw')))
+              .catch(e => done());
+          });
+        })
+    });
+  });
+
+  describe('then mode', function () {
+    it('split err from callbacks', function () {
+      _(null, function (res) {
+        assert.equal(res, 1);
+      })(null, 1);
+
+      _(function (e) {
+        assert.equal(e.message, 'err');
+      })(Error('err'));
+    });
+
+    it('support multiple params', function () {
+      _(null, function (res, r2, r3) {
+        assert.equal(res, 1);
+        assert.equal(r2, 2);
+        assert.equal(r3, 3);
+      })(null, 1, 2, 3);
+    });
+  });
+});
